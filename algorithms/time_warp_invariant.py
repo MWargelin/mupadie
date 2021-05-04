2372652a4c02acd7dc335a2e2c4a2aa3a7593681
MWargelin <matias.wargelin@gmail.com>
2021-05-04 12:26:35 +0300

    Improve documentation of the time-warp invariant algorithm

--- algorithms/time_warp_invariant.py
+++ algorithms/time_warp_invariant.py
@@ -10,21 +10,29 @@ def compute(point_set, window=0):
 		consecutive notes in a pattern. If window is set to 0, window
 		is set to unrestricted mode, where there can be arbitrarily many
 		notes between consecutive notes.
+
+	Returns:
+		Patterns discovered from the given point set using a time-warp
+		and transposition invariant algorithm
 	"""
 	point_set.sort()
 	relabeled_set, mapping = _relabel_x(point_set)
 	note_pairs = _note_pair_groups(relabeled_set)
 	patterns = []
 
+	# window = 0 means unrestricted mode. Use length of the point set
+	# as the window if this is the case, as that is enough for the window
+	# to be unrestricted in practise.
 	if window == 0:
 		_window = len(point_set)
 	else:
 		_window = window
 
+	# Find patterns
 	for group in note_pairs.values():
 		patterns.append(_process_group(group, _window))
 
-	# Map notes back to the original notes
+	# Map notes in patterns back to the original notes
 	for instances in patterns:
 		for i in range(len(instances)):
 			mapped = [mapping[note] for note in instances[i]]
@@ -38,9 +46,17 @@ def _relabel_x(point_set):
 
 	The note onsets relabeled to be integers in between 1 and the length of
 	the point set. Returns the relabeled set, and a mapping dictionary with
-	relabeled notes as keys and original points as the values.
+	relabeled notes as keys and original points as the values. Notes played
+	originally at the same time get the same note onset value.
 
 	Assumes that the given point set is sorted.
+
+	Args:
+		point_set: the point set to relabel
+
+	Returns:
+		a tuple: the point set relabeled, and a mapping dictionary with the
+		relabeled points as keys and the original points as values
 	"""
 	relabeled_set = []
 	mapping = {}
@@ -60,6 +76,17 @@ def _relabel_x(point_set):
 
 
 def _note_pair_groups(point_set):
+	"""Create all combinations of two notes in the point set and
+	group them according to the interval between the note pair.
+
+	Args:
+		point_set: point set to use
+
+	Returns:
+		A dictionary with intervals as keys, with list of all note pairs in
+		the given point set where the interval between the pair is the interval
+		in the key as value
+	"""
 	note_pairs = defaultdict(list)
 
 	for x in point_set:
@@ -73,6 +100,19 @@ def _note_pair_groups(point_set):
 
 
 def _process_group(group, window):
+	"""Find the longest pattern from given group
+
+	Args:
+		group: group of note pairs to find the longest pattern
+		from
+		window: the number of notes there can be in between
+		consecutive notes of a pattern. Notes played at the
+		same time are counted as "one note".
+
+	Returns:
+		The longest pattern in the group as a list of two
+		items (first and second instance of the pattern)
+	"""
 	group.sort(key=lambda pair: (pair[0][0], pair[1][0]))
 
 	arr = [(pair[0][0], pair[1][0]) for pair in group]
@@ -107,8 +147,18 @@ def _process_group(group, window):
 
 
 def _format_for_visualisation(patterns, window):
+	"""Transform the pattern discovery results into the specified data
+	structure used in the Mupadie visualisation.
+
+	Args:
+		patterns: the discovered patterns
+		window: the window parameter as set by the user of the application
+
+	Returns:
+		A dictionary documenting the pattern discovery results
+	"""
 	if window == 0:
-		window_string = "unrestricted"
+		window_string = 'unrestricted'
 	else:
 		window_string = window
 

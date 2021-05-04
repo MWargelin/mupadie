62bb1e2bd496f2a9217142bd4e42612eb5df5f13
MWargelin <matias.wargelin@gmail.com>
2021-05-04 09:29:09 +0300

    Fix bug when not selecting the first track of a file
    There was a bug that tempo messages were lost if the user did not
    choose to have the first track of a file in the point set,
    resulting in all notes happening at time 0. Filtering not chosen
    tracks is changed to avoid this: now the velocity of all notes
    in not chosen tracks is turned to 0.

--- read_midi/read_midi.py
+++ read_midi/read_midi.py
@@ -4,9 +4,7 @@ import mido
 def to_point_set(path, chosen_tracks):
 	point_set = set()
 	mid = mido.MidiFile(path)
-
-	mid.tracks = [track for i, track in enumerate(mid.tracks) if i in chosen_tracks]
-	mid = mido.merge_tracks(mid.tracks)
+	mid = _filter_tracks(mid, chosen_tracks)
 
 	note_onset = 0
 	tempo = 0
@@ -18,6 +16,21 @@ def to_point_set(path, chosen_tracks):
 			point_set.add((note_onset, msg.note))
 
 	return sorted(point_set)
+
+
+def _filter_tracks(mid, chosen_tracks):
+	"""Turn the velocity of all notes in not chosen tracks to 0,
+	so that the notes don't get picked up when reading the point set.
+	This is done to avoid having to delete the tracks completely -
+	deleting the tracks leads to a bug when removing the first track
+	of a file.
+	"""
+	for i, track in enumerate(mid.tracks):
+		if i not in chosen_tracks:
+			for msg in track:
+				if msg.type == 'note_on':
+					msg.velocity = 0
+	return mid
 
 
 def tracks_enumerate(path):

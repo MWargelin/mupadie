e3d2df26fba4a5ce1f7f85491b291bd3ba9c1a1b
MWargelin <matias.wargelin@gmail.com>
2021-05-05 19:07:50 +0300

    Add info tags with tooltips to describe algorithms and parameters

--- static/dynamic_form.js
+++ static/dynamic_form.js
@@ -7,6 +7,12 @@ $(document).ready(function(){
             $("#algorithms").append(timewarp_div)
         }
 
+        // To show the tooltips of info tags, the tooltips have to be registered again
+        var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+        var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+        })
+
         $(".remove-button").click(function(){
             $(this).parent().remove();
         });
@@ -15,22 +21,30 @@ $(document).ready(function(){
 
 var siatec_div = '\
 <div name="siatec-parameters" class="mb-3 form-control">\
-    <h4 class="mb-3">SIATEC</h4>\
+    <h4 class="mb-3">SIATEC\
+    <i class="bi bi-info-circle text-secondary" data-bs-toggle="tooltip" data-bs-placement="right" title="SIATEC finds the maximal translatable pattern (MTP) for every possible translation vector of a piece."></i>\
+    </h4>\
     <input type="hidden" name="algorithm_name" value="SIATEC">\
     <div class="mb-2">\
         <input type=number class="form-control" name="siatec-min-pattern-length" value=2 min=2>\
-        <label for="siatec-min-pattern-length" class="form-label">Minimum pattern length</label>\
+        <label for="siatec-min-pattern-length" class="form-label">Minimum pattern length \
+        <i class="bi bi-info-circle text-secondary" data-bs-toggle="tooltip" data-bs-placement="right" title="Patterns with less notes than this number are discarded. Raising this number can lead to shorter computation times."></i>\
+        </label>\
     </div>\
     <button class="remove-button btn btn-outline-danger mb-2">Remove</button>\
 </div>'
 
 var timewarp_div = '\
 <div name="timewarp-parameters" class="mb-3 form-control">\
-    <h4 class="mb-3">time-warp and transposition invariant algorithm</h4>\
+    <h4 class="mb-3">time-warp and transposition invariant algorithm\
+    <i class="bi bi-info-circle text-secondary" data-bs-toggle="tooltip" data-bs-placement="right" title="An algorithm that tolerates distortion in the note onsets, meaning that different instances of a pattern can have differing rhythms."></i>\
+    </h4>\
     <input type="hidden" name="algorithm_name" value="time-warp-invariant">\
     <div class="mb-2">\
         <input type=number class="form-control" name="timewarp-window" value=0 min=0>\
-        <label for="timewarp-window" class="form-label">window</label>\
+        <label for="timewarp-window" class="form-label">window \
+        <i class="bi bi-info-circle text-secondary" data-bs-toggle="tooltip" data-bs-placement="right" title="The number of notes (that don\'t belong to the pattern) there can be in between consecutive notes of a pattern instance. Notes played at the same time are counted as \'one note\' by this parameter. Setting window to 0 means unrestricted mode, where there can be arbitrary many notes in between notes of a pattern."></i>\
+        </label>\
     </div>\
     <button class="remove-button btn btn-outline-danger mb-3">Remove</button>\
 </div>'
